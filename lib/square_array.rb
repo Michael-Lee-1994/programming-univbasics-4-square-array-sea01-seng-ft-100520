16170169e33a1ef1dbdc3ce99d51791febacfe2b
Michael Lee <michael.l33.1994@gmail.com>
2020-09-21 19:33:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,13 +4,5 @@ def square_array(array)
   array.length.times { |index|
     new.array.push(array[index]**)
   }
+  new_array
 end
-
-
-array = [1,2,3,4,5]
-new_array = []
- 
-array.length.times do |index|
-  new_array.push(array[index] + 1)
-end
-new_array
